38c3655470a49ee8a025e4cc6678618170367e7b
vojte <vojte@PC-DOMA>
2025-01-27 14:23:38 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 0;
+        while (b < 34)
+        {
+            Console.WriteLine(b);
+            b = b + a;
+            Console.WriteLine(a);
+            a = a + b;
+        }
+    }
+}
